c8729a26f59286e8df685f7a8167f8c29b806e10
ozgur <ozgur@ozgurs-MacBook-Pro.local>
2023-11-17 17:23:16 +0300

    heart icon is converted to svg

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Character } from '../types/characters';
-import FavoriteIcon from '@mui/icons-material/Favorite';
+import whiteHeartIcon from '../assets/white-heart.svg'
+import redHeartIcon from '../assets/red-heart.svg'
+import styles from '../styles/CharacterCard.module.scss'
 
 type CharacterProps = {
   character: Character;
@@ -16,17 +18,16 @@ const CharacterCard: React.FC<CharacterProps> = ({ character }) => {
   };
   return (
     <Link to={`/characters/${character.id}`}>
-      <div style={{ position: 'relative' }}>
+      <div className={styles.card}>
         <img src={character.image} alt={character.name} />
-        <FavoriteIcon
-          style={{
-            position: 'absolute',
-            top: 0,
-            right: 0,
-          }}
-          color={isHeartClicked ? 'error' : 'action'}
-          onClick={handleHeartClick}
-        />
+        <img 
+        src={isHeartClicked ? redHeartIcon : whiteHeartIcon} 
+        alt="heart" 
+        onClick={handleHeartClick} 
+        style={{ position: 'absolute', top: 10, right: 10 }}
+        height={40}
+        width={40} 
+         />
         <h2>{character.name}</h2>
         <p>Status: {character.status}</p>
         <p>Species: {character.species}</p>
