81ae4496267603e2972b8d6d84d453e9a25d4fe8
ozgur <ozgur@ozgurs-MacBook-Pro.local>
2023-11-29 13:00:35 +0300

    header responsive design is complete

--- src/pages/Characters.tsx
+++ src/pages/Characters.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import CharacterCard from '../components/CharacterCard'
-import useCharacters from '../utils/useCharacters'
+import useCharacters from '../hooks/useCharacters'
 import styles from '../styles/pages/Characters.module.scss'
 import Pagination from '../components/Pagination'
 import { useSelector } from 'react-redux'
@@ -25,32 +25,47 @@ const Characters: React.FC = () => {
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        <div className={styles.status}>
+        <div className={styles.titles}>
           <h2>Filter by Status</h2>
+          <Link className={styles.favLink} to="/favorites">Go to My Favorites</Link>
+        </div>
+        <div className={styles.buttonsContainer}>
           <div className={styles.buttons}>
-            <button className={styles.alive} onClick={() => setSelectedStatus('Alive')}>
+            <button
+              className={`${styles.alive} ${selectedStatus === 'Alive' ? styles.clicked : ''}`}
+              onClick={() => selectedStatus === 'Alive' ? setSelectedStatus(null) : setSelectedStatus('Alive')}
+            >
               <span className={styles.circle}></span> Alive
             </button>
-            <button className={styles.dead} onClick={() => setSelectedStatus('Dead')}>
+            <button
+              className={`${styles.dead} ${selectedStatus === 'Dead' ? styles.clicked : ''}`}
+              onClick={() => selectedStatus === 'Dead' ? setSelectedStatus(null) : setSelectedStatus('Dead')}
+            >
               <span className={styles.circle}></span> Dead
             </button>
-            <button className={styles.unknown} onClick={() => setSelectedStatus('Unknown')}>
+            <button
+              className={`${styles.unknown} ${selectedStatus === 'Unknown' ? styles.clicked : ''}`}
+              onClick={() => selectedStatus === 'Unknown' ? setSelectedStatus(null) : setSelectedStatus('Unknown')}
+            >
               <span className={styles.circle}></span> Unknown
             </button>
-            {selectedStatus && (
-              <button className={styles.clear} onClick={() => setSelectedStatus(null)}>
-                <span className={styles.circle}></span> Clear
-              </button>
-            )}
+
+            {/* {selectedStatus && (
+                <button className={styles.clear} onClick={() => setSelectedStatus(null)}>
+                  <span className={styles.circle}></span> Clear
+                </button>
+              )} */}
+
           </div>
         </div>
-        <Link className={styles.favLink} to="/favorites">Go to My Favorites</Link>
       </div>
       <div className={styles.charactersContainer}>
         <div className={styles.characters}>
-          {currentCharacters.map((character) => (
-            <CharacterCard key={character.id} character={character} />
-          ))}
+          {currentCharacters.length > 0 ?
+            currentCharacters.map((character) => (
+              <CharacterCard key={character.id} character={character} />
+            ))
+            : <div className={styles.noCharacters}>No characters found</div>}
         </div>
       </div>
       <Pagination items={charactersByStatus} paginationFor='characters' itemsPerPage={charactersPerPage} />
