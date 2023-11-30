d39643ff99c544c5b780251861c7a87573483137
ozgur <ozgur@ozgurs-MacBook-Pro.local>
2023-11-30 17:28:09 +0300

    locations per change number changed

--- src/pages/Locations.tsx
+++ src/pages/Locations.tsx
@@ -19,7 +19,7 @@ const Locations: React.FC = () => {
   const locations = useSelector((state: RootState) => state.locations.locations)
   const currentLocations = useSelector((state: RootState) => state.locations.currentLocations)
   const isLoading = useSelector((state: RootState) => state.locations.loading)
-  const locationsPerPage = 3
+  const locationsPerPage = 4
 
   if (isLoading) {
     return <div>Loading...</div>
